d078e24d0a8b573c3f8797fbf577f90a8d45bf85
Raphael Tomaz <rphl.tomas@gmail.com>
2022-10-25 22:46:23 +0100

    Messenger connect to server funcionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import './App.scss';
-import History from './components/History'
-import Chat from './components/Chat'
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import Messages from "./components/Messages";
+import MessageInput from "./components/MessageInput";
+
+import "./App.scss";
+import History from "./components/History";
+import Chat from "./components/Chat";
 
 function App() {
-  return (
-    <div className='appContainer'>
-      <div className='content'>
-        <History/>
-        <Chat/>
-      </div>
+  const [socket, setSocket] = useState(null);
 
+  useEffect(() => {
+    const newSocket = io(`http://${window.location.hostname}:3000`);
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [setSocket]);
+
+  return (
+    <div className="App">
+      <header className="app-header">React Chat</header>
+      {socket ? (
+        <div className="chat-container">
+          <Messages socket={socket} />
+          <MessageInput socket={socket} />
+        </div>
+      ) : (
+        <div>Not Connected</div>
+      )}
+
+      <div className="appContainer">
+        <div className="content">
+          <History />
+          <Chat />
+        </div>
+      </div>
     </div>
-    )
+  );
 }
 
 export default App;
